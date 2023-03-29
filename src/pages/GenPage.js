7a821d297626e4c19bf4096b9c1919795b42a48e
Ivankub <vanadolgun@gmail.com>
2023-03-29 21:48:20 +0700

    big ge-algo front update
    function "findShortestRouteGen" is connected to front
    
    you can:
    ->set from 1 to 15 points
    ->choose generations count
    ->choose chance of evolution
    ->switch drawing speed between 3 modes
    
    all functions commented
    all code separated by functions
    
    best way of each iteration is drawing in the end of iteration
    
    best way ever is drawing at the end of all
    
    there are 8 default colors fow ways to understand changing of route
    
    one more style for best way on iteration
    
    and one more for best way of all ways

--- src/pages/GenPage.js
+++ src/pages/GenPage.js
@@ -10,6 +10,12 @@ const GenPage = () => {
                 <div id="header">
                     <div id="naming" className="unselectable">Gen algorithm</div>
                     <div id="points_counter" className="unselectable">0</div>
+                    <input id="gens_counter" className="unselectable" type={"number"}></input>
+                    <div id="generations" className="unselectable">generations</div>
+                    <input id="evolution_changer" className="unselectable" type={"number"}></input>
+                    <div id="evolution" className="unselectable">evolve chance (%)</div>
+                    <div id="speed_mode" className="unselectable false">slow mode</div>
+                    <div id="start_button" className="unselectable">start</div>
                 </div>
                 <div id="container">
                     <div id="pointing_box">
