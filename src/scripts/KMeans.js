4e23adbdb6ecfca35ad1b474b9f79151904793cc
serachkovivan <serachkovivan003@gmail.com>
2023-03-29 10:15:12 +0700

    added comments
    and delete some useless fitures

--- src/scripts/KMeans.js
+++ src/scripts/KMeans.js
@@ -13,13 +13,13 @@ class KMeans {
     dotG;
     centerG;
 
-    constructor(circleNumber, clusterNumber){
-       this.initArea();
-        this.init(circleNumber, clusterNumber);
+    constructor(dotNumber, clusterNumber){ //получает
+        this.initArea();
+        this.init(dotNumber, clusterNumber);
         this.draw();
     }
-    
-     initArea(){
+
+    initArea(){   // инициализация пространства (чисто визуал кроме одной строки)
         const that = this;
         this.flag = false;
         this.WIDTH = d3.select("#kmeans")[0][0].clientWidth - 20;
@@ -31,102 +31,99 @@ class KMeans {
             .style('background', '#83abd4')
             .style('cursor', 'pointer')
             .on('click', function() {
-                d3.event.preventDefault();
-                that.step();
+                // d3.event.preventDefault();
+                that.step(); // кроме этой строки - нажимая на плоскость совершается шаг работы алгоритма
             });
 
-        this.lineG = this.svg.append('g');
-        this.dotG = this.svg.append('g');
-        this.centerG = this.svg.append('g');
+        this.lineG = this.svg.append('g');   // эти переменные таскают визуал,
+        this.dotG = this.svg.append('g');    // тоже не имеют практического значения
+        this.centerG = this.svg.append('g'); //   |
     }
 
-    init(circleNumber, clusterNumber) {
-        this.clusterNumber = circleNumber;
-        this.dotsNumber = clusterNumber;
+    init(dotNumber, clusterNumber) {   // на вход от пользователя нужно получить количество только точек
+        this.dotsNumber = dotNumber || 9; // на случай, если от пользователя не пришли данные
+        this.clusterNumber = clusterNumber || 35;  // на случай, если от пользователя не пришли данные
 
-        this.initGroups();
-        this.initDots();
+        this.initGroups();  // переход к инициализации групп (кластерных центров)
+        this.initDots();    // переход к инициализации точек (кластерные элементы)
     }
 
-    initGroups(){
+    initGroups(){   // инициализация групп (кластерных центров)
         this.clusters = [];
         for (let i = 0; i < this.clusterNumber; i++) {
-          let g = {
-            id: 'group_'+i,
-            dots: [],
-            color: 'hsl(' + (i * 360 / this.clusterNumber) + ',100%,50%)',
-            center: {
-              x: Math.random() * this.WIDTH,
-              y: Math.random() * this.HEIGHT
-            },
-            init: {
-              center: {}
-            }
-          };
-          g.init.center = {
-            x: g.center.x,
-            y: g.center.y
-          };
-          this.clusters.push(g);
+            let g = {
+                id: 'group_'+i,   // это потом идет на вывод в консоль - не нужно
+                dots: [],
+                color: 'hsl(' + (i * 360 / this.clusterNumber) + ',100%,50%)', // цвет в hsl
+                center: {
+                    x: Math.random() * this.WIDTH, // рандомная задача центра
+                    y: Math.random() * this.HEIGHT // теоретически, можно задавать как рандомно центры
+                },                                 // так и намеренно, как и точки (элементы) кластера
+            };
+            g.center = {
+                x: g.center.x,
+                y: g.center.y
+            };
+            this.clusters.push(g);
 
         }
-        console.log('clusters: ', this.clusters);
+        //console.log('clusters: ', this.clusters); // вывод в консоль кластерных центров
     }
 
-    initDots(){
+    initDots(){ // инициализация точек (кластерных элементов)
         this.dots = [];
         this.flag = false;
         for (let i = 0; i < this.dotsNumber; i++) {
-          let dot ={
-            x: Math.random() * this.WIDTH,
-            y: Math.random() * this.HEIGHT,
-            group: undefined
-          };
-          dot.init = {
-            x: dot.x,
-            y: dot.y,
-            group: dot.group
-          };
-          this.dots.push(dot);
+            let dot ={
+                x: Math.random() * this.WIDTH,  // случайная задача координат точек
+                y: Math.random() * this.HEIGHT, // необходимо получать от пользователя координаты, куда он жмет
+                                                // на плоскости
+                group: undefined // изначально, ни одна точка не принадлежит ни одному кластеру
+            };
+            dot.init = {
+                x: dot.x,
+                y: dot.y,
+                group: dot.group
+            };
+            this.dots.push(dot);
         }
-        console.log('dots: ', this.dots);
+        // console.log('dots: ', this.dots);  // вывод в консоль всех точек
     }
 
-    step() {
-        if (this.flag) {
-          this.moveCenter();
+    step() {                       // основной код
+        if (this.flag) {           // в первый раз флаг ложен, поэтому начинается с else
+            this.moveCenter();
         } else {
-          this.updateGroups();
+            this.updateGroups();
         }
-        this.draw();
+        this.draw();               // визуал - можно делать, что угодно
         this.flag = !this.flag;
     }
 
-    draw() {
-        this.drawCircles();
+    draw() {                // визуальная часть, которую можно менять
+        this.drawCircles(); // уходит рисовать точки (элементы) кластеров
 
-        if (this.dots[0].group) {
-            //lines array from dots to centers
+        if (this.dots[0].group) { // если у точки нет цвета, и линий быть не должно
             this.drawLines();
         } else {
             this.lineG.selectAll('line').remove();
-        }
+        }                        // линии тоже не являются необходимой частью - их можно вообще убрать
 
         let c = this.centerG.selectAll('path').data(this.clusters);
-        let updateCenters = function(centers) {
+        let updateCenters = function(centers) { // работа с визуалом центров
             centers
-            .attr('transform', function(d) { return "translate(" + d.center.x + "," + d.center.y + ") rotate(45)";})
-            .attr('fill', function(d,i) { return d.color; })
-            .attr('stroke', '#aabbcc');
+                .attr('transform', function(d) { return "translate(" + d.center.x + "," + d.center.y + ") rotate(45)";})
+                .attr('fill', function(d,i) { return d.color; })
+                .attr('stroke', '#aabbcc');
         };
         c.exit().remove();
         updateCenters(c.enter()
-          .append('path')
-          .attr('d', d3.svg.symbol().type('cross'))
-          .attr('stroke', '#aabbcc'));
+            .append('path')
+            .attr('d', d3.svg.symbol().type('cross'))
+            .attr('stroke', '#aabbcc'));
         updateCenters(c
-          .transition()
-          .duration(500));
+            .transition()
+            .duration(500));
     }
 
     drawCircles(){
@@ -134,24 +131,24 @@ class KMeans {
         circles.enter().append('circle');
         circles.exit().remove();
         circles
-          .transition()
-          .duration(1000)
-          .attr('cx', function(d) { return d.x; })
-          .attr('cy', function(d) { return d.y; })
-          .attr('fill', function(d) { return d.group ? d.group.color : '#ffffff'; })
-          .attr('r', 10);
+            .transition()
+            .duration(1000)
+            .attr('cx', function(d) { return d.x; })
+            .attr('cy', function(d) { return d.y; })
+            .attr('fill', function(d) { return d.group ? d.group.color : '#ffffff'; })
+            .attr('r', 10);
     }
 
     drawLines(){
         let l = this.lineG.selectAll('line').data(this.dots);
         let updateLine = function(lines) {
 
-        lines
-          .attr('x1', function(d) { return d.x; })
-          .attr('y1', function(d) { return d.y; })
-          .attr('x2', function(d) { return d.group.center.x; })
-          .attr('y2', function(d) { return d.group.center.y; })
-          .attr('stroke', function(d) { return d.group.color; });
+            lines
+                .attr('x1', function(d) { return d.x; })
+                .attr('y1', function(d) { return d.y; })
+                .attr('x2', function(d) { return d.group.center.x; })
+                .attr('y2', function(d) { return d.group.center.y; })
+                .attr('stroke', function(d) { return d.group.color; });
         };
         (l.enter().append('line'));
         updateLine(l.transition().duration(500));
@@ -185,11 +182,9 @@ class KMeans {
         });
 
         if (finished){
-            console.log('Algorithm is finished');
-            console.log('clusters', this.clusters);
-            console.log('dots', this.dots);
-        }
-      }
+            console.log('Algorithm is finished'); // тут возникает маленький диссонанс в определении
+        }                                         // идет проверка по первому только кластеру - то есть
+    }                                             // если другие смещаются, то все равно может выдать, что алгоритм закончен
 
     updateGroups() {
         const that = this;
@@ -211,3 +206,4 @@ class KMeans {
         });
     }
 }
+// это еще не все комментарии
