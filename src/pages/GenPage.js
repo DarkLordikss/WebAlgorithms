ee2211f27a93b2e0ac656d1eef8c3812447cb6c0
Ivankub <vanadolgun@gmail.com>
2023-03-30 20:35:17 +0700

    den algo front update
    whats new:
    1. population size could be changed now
    2. you cant do anything while lines are drawing
    3. "skip animation" button
    
    whats changed:
    1. default colors are now have little shadows
    2. background color of the point box is darker and more purple
    3. generations limit (it wasnt shows, but limit was 100, now it is 100000)
    
    thats all)
    
    all code is splitted by functions as always
    
    all functions are commented (as always too)

--- src/pages/GenPage.js
+++ src/pages/GenPage.js
@@ -10,17 +10,23 @@ const GenPage = () => {
                 <div id="header">
                     <div id="naming" className="unselectable">Gen algorithm</div>
                     <div id="points_counter" className="unselectable">0</div>
+
                     <input id="gens_counter" className="unselectable" type={"number"}></input>
                     <div id="generations" className="unselectable">generations</div>
+
                     <input id="evolution_changer" className="unselectable" type={"number"}></input>
-                    <div id="evolution" className="unselectable">evolve chance (%)</div>
+                    <div id="evolution" className="unselectable">mutatt chance (%)</div>
+
+                    <input id="popSize_changer" className="unselectable" type={"number"}></input>
+                    <div id="population" className="unselectable">population size</div>
+                    
                     <div id="speed_mode" className="unselectable false">slow mode</div>
+                    <div id="skip_button" className="unselectable false">skip animation</div>
                     <div id="start_button" className="unselectable">start</div>
                 </div>
                 <div id="container">
-                    <div id="pointing_box">
-                        
-                    </div>
+                    <div id="pointing_box"></div>
+                    <div id="evol_loader" className="unselectable">evolving...</div>
                 </div>
             </div>
     );
