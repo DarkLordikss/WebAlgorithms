f478506ad6d43bb62624452f89aa26f014363d64
DarkLordikss <75681846+DarkLordikss@users.noreply.github.com>
2023-04-05 22:25:54 +0700

    Decision tree (basic)

--- src/server/server.py
+++ src/server/server.py
@@ -2,19 +2,40 @@ from flask import Flask, request
 import os
 
 from neuro.network import neuro_network
+from tree.tree import make_model, do_a_decision
 
 
 app = Flask(__name__)
 
 
 @app.route('/determine_digit', methods=['GET'])
-def index():
+def neuro():
     filepath = request.args.get("filepath")
 
     if not os.path.exists(filepath):
         return "404 file not exist"
 
     return neuro_network(filepath)
+
+
+@app.route('/make_tree', methods=['GET'])
+def make_tree():
+    filepath = request.args.get("filepath")
+
+    if not os.path.exists(filepath):
+        return "404 file not exist"
+
+    return make_model(filepath)
+
+
+@app.route('/get_decision', methods=['GET'])
+def make_decision():
+    row = request.args.get("row").split(",")
+
+    if not os.path.exists("tree/model/model.json"):
+        return "404 model not exist"
+
+    return do_a_decision(row)
 
 
 if __name__ == '__main__':
